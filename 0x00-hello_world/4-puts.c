941659e71cac657a8885204f96a0dfa7b9a34e9a
AnassZr23 <anass.zeroual@etu.uae.ac>
2023-10-05 17:15:46 +0000

    first code C_programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -  C program that prints exactly :Programming is like
+ *
+ * building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle,");
+return (0);
+}
