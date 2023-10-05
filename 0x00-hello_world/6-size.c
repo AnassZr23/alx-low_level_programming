d69ba5197fae5e9d797f936210dfb7d180704a21
AnassZr23 <anass.zeroual@etu.uae.ac>
2023-10-05 19:11:39 +0000

    2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ printf("Size of char: %lu byte(s)\n", sizeof(char));
 printf("Size of int: %lu byte(s)\n", sizeof(int));
 printf("Size of long int: %lu byte(s)\n", sizeof(long));
 printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-printf("Size of float %lu byte(s)\n", sizeof(float));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
 
 
 return (0);
