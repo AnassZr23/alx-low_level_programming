f12fe16b54f7c96beacfc8a23c22164761ab6027
AnassZr23 <anass.zeroual@etu.uae.ac>
2023-10-05 17:48:25 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -  C program that prints exactly
+ *
+ * Programming is like building a multilingual puzzle
+ *
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
