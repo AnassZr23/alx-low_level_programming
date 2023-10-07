a0735a1465313c374de5929f5424b44cd83f950d
AnassZr23 <anass.zeroual@etu.uae.ac>
2023-10-07 15:47:32 +0000

    while loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main -print_alphabet.c
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter = 'a';
+char LETTER = 'A';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+while (LETTER <= 'Z')
+{
+putchar(LETTER);
+LETTER++;
+}
+putchar('\n');
+return (0);
+}
+
